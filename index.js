20ee2c69e0f10a04430f4be7d86718aa713645bc
ar3shoaib <ar.shoaib16@gmail.com>
2023-01-12 21:48:22 -0500

    Player object and Map

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ canvas.setAttribute("width", SCREEN_WIDTH)
 canvas.setAttribute("height", SCREEN_HEIGHT)
 // Append it on the page
 document.body.appendChild(canvas)
+
+const TICK = 30;
+// Allows to draw on the canvas, provides the api for it
+const context = canvas.getContext("2d")
+
+function ClearScreen(){
+    context.fillStyle = "red"
+    context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+}
+function MovePlayer(){}
+function GetRays(){}
+function RenderScene(rays){}
+function RenderMinimap(x, y, scale, rays){}
+
+// Game and rendering logic 
+function Gameloop() {
+    // Clear screen and redraw with new values
+    ClearScreen()
+
+    // First person 
+    MovePlayer()
+
+    // Calculate and cast the rays and render them on the scene (3D)
+    const rays = GetRays()
+    RenderScene(rays)
+    // Then render the rays on the minimap (receives position and scale)
+    RenderMinimap(0, 0, 0.75, rays)
+}
+
+// Every tick (30ms), execute gameloop function
+setInterval(Gameloop, TICK)
