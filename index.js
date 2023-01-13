18d115aeefe426e54d9bbb7dcb8b46d9c75e869e
ar3shoaib <ar.shoaib16@gmail.com>
2023-01-12 21:50:18 -0500

    Update Player object and Map

--- index.js
+++ index.js
@@ -15,14 +15,43 @@ document.body.appendChild(canvas)
 const TICK = 30;
 // Allows to draw on the canvas, provides the api for it
 const context = canvas.getContext("2d")
+// For map representation to render it
+const CELL_SIZE = 64;
+
+// Each row will contain horizontal cells and main array will be array of those rows
+// 1 for wall, 0 for empty cells
+const map = [
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1], 
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+];
+
+// For player position, velocity, and direction/angle (in radians)
+const player = {
+    // Obtain the position in terms of the map (each cell is 64 units)
+    // Located in the middle of the second cell (1,1)
+    x: CELL_SIZE * 1.5,
+    // (1,2)
+    y: CELL_SIZE * 2, 
+    angle: 0,
+    speed: 0
+}
 
 function ClearScreen(){
     context.fillStyle = "red"
     context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 }
+
 function MovePlayer(){}
-function GetRays(){}
+function GetRays(){
+    return []
+}
 function RenderScene(rays){}
+// x,y positions of the minimap on the screen, scale of map projection, rays is the array of rays
 function RenderMinimap(x, y, scale, rays){}
 
 // Game and rendering logic 
